a5ded17db89d372489ebb59513935e100212839a
Kitti Pariyaakkarakul <zondezatera@gmail.com>
2019-12-11 03:39:10 +0700

    add routing

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
 import React from 'react'
 import {
   View,
+  Text
 } from 'react-native'
+import 'react-native-gesture-handler'
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
 
 import { Board } from './src/features'
 
-const App = () => (
-  <View style={{ alignItems: 'center' }}>
-    <Board />
+// const App = () => (
+//   <View style={{ alignItems: 'center' }}>
+//     <Board />
+//   </View>
+// )
+const HomeScreen = () => (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>Home Screen</Text>
   </View>
 )
 
-export default App
+const App = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+  },
+})
+
+export default createAppContainer(App)
